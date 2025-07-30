df1b8843c12a00d4e6f40aba1399416ed5a2b523
Katya Weicht <katya.weicht@gmail.com>
2025-07-30 16:52:26 -0600

    Complete valid parentheses method using stack logic

--- stack.py
+++ stack.py
@@ -4,4 +4,25 @@ def is_valid_parentheses(s: str) -> bool:
     Only (), {}, and [] are considered valid.
     """
     # TODO: Implement stack logic to validate parentheses
-    pass
+    stack = []
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        #if the last character in the string makes it a valid parentheses, remove it 
+        elif len(stack) > 0 and char == ')' and stack[-1] == '(':
+            stack.pop()
+        elif char == '{':
+            stack.append(char)
+        elif len(stack) > 0 and char == '}' and stack[-1] == '{':
+            stack.pop()
+        elif char == '[':
+            stack.append(char)
+        elif len(stack) > 0 and char == ']' and stack[-1] == '[':
+            stack.pop()
+
+    #if the list is empty it means all the parentheses were valid because they were all removed
+    if len(stack) == 0:
+        return True
+    else:
+        False
+
